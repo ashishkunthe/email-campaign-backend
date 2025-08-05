4823b868d8ac2e772437713be21ab7ea9f785458
ashishkunthe <ashishkunthe2003@gmail.com>
2025-08-05 20:02:14 +0530

    added db configs

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
+import { MongoDBConnection } from "./configs/db";
+
+dotenv.config();
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello Sir");
 });
+
+MongoDBConnection();
 
 app.listen(5000, () => {
   console.log("server is runnig on post 5000");
