80728a91bfe5b244ad0a018a129e3e18cb3a9228
Ashish Kunthe <160906505+ashishkunthe@users.noreply.github.com>
2025-08-05 16:38:06 +0530

    Delete dist directory

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const app = express();
-app.get("/", (req, res) => {
-    res.send("Hello Sir");
-});
-app.listen(5000, () => {
-    console.log("server is runnig on post 5000");
-});
-//# sourceMappingURL=index.js.map
