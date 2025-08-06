2a431f1c4318395b87a60fb21652d948ecb5542f
ashishkunthe <ashishkunthe2003@gmail.com>
2025-08-06 11:09:13 +0530

    added rate limit

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,21 @@ import express from "express";
 import dotenv from "dotenv";
 import { MongoDBConnection } from "./configs/db.js";
 import authRoutes from "./routes/authRoutes.js";
+import rateLimit from "express-rate-limit";
+
+const globalLimiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 100,
+  message: "Too many requests, please slow down.",
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
 dotenv.config();
 
 const app = express();
 
+app.use(globalLimiter);
 app.use("/api/auth", authRoutes);
 
 app.listen(5000, async () => {
