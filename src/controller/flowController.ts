acdae027457f11c725b472f92f5c9e35e5ac13c2
ashishkunthe <ashishkunthe2003@gmail.com>
2025-08-06 21:09:32 +0530

    added additional functions like delete and update

--- src/controller/flowController.ts
+++ src/controller/flowController.ts
@@ -5,7 +5,7 @@ interface RequestUpdated extends Request {
   userId: string;
 }
 
-// createFlow – Save new flow (POST)
+// createFlow
 export async function createFlow(req: RequestUpdated, res: Response) {
   const { name, nodes, edges } = req.body;
   const userId = req.userId;
@@ -27,7 +27,7 @@ export async function createFlow(req: RequestUpdated, res: Response) {
   }
 }
 
-//  getAllFlows – Return all flows for current user (GET)
+//  getAllFlows
 export async function getAllFlows(req: RequestUpdated, res: Response) {
   const userId = req.userId;
 
@@ -51,9 +51,86 @@ export async function getAllFlows(req: RequestUpdated, res: Response) {
     });
   }
 }
-//  getFlowById – Return a single flow (GET by ID)
-export async function getFlowById(req: RequestUpdated, res: Response) {}
-//  updateFlow – Update existing flow (PUT)
-export async function updateFlow(req: RequestUpdated, res: Response) {}
+//  getFlowById
+export async function getFlowById(req: RequestUpdated, res: Response) {
+  const userId = req.userId;
+  const flowId = req.params.id;
+
+  try {
+    const flow = await flowModels.findById(flowId);
+
+    if (!flow || flow.userId.toString() !== userId) {
+      return res.status(404).json({
+        message: "Flow not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "Flow sucessfully fetched",
+      flow: flow,
+    });
+  } catch (error) {
+    console.log("Error fetching requested flow", error);
+    res.status(500).json({
+      message: "Error fetching requested flow",
+    });
+  }
+}
+//  updateFlow
+export async function updateFlow(req: RequestUpdated, res: Response) {
+  const userId = req.userId;
+  const flowId = req.params.id;
+  const { name, nodes, edges } = req.body;
+
+  try {
+    const existingFlow = await flowModels.findById(flowId);
+
+    if (!existingFlow || existingFlow.userId.toString() !== userId) {
+      return res.status(404).json({
+        message: "Flow not found",
+      });
+    }
+
+    existingFlow.name = name;
+    existingFlow.nodes = nodes;
+    existingFlow.edges = edges;
+
+    await existingFlow.save();
+
+    res.status(200).json({
+      message: "Flow update successful",
+    });
+  } catch (error) {
+    console.log("Unable to update the flow", error);
+    res.status(500).json({
+      message: "Unable to update the flow",
+    });
+  }
+}
+
 //  deleteFlow
-export async function deleteFlow(req: RequestUpdated, res: Response) {}
+export async function deleteFlow(req: RequestUpdated, res: Response) {
+  const userId = req.userId;
+  const flowId = req.params.id;
+
+  try {
+    const findFlow = await flowModels.findById(flowId);
+
+    if (!findFlow || findFlow.userId.toString() !== userId) {
+      return res.status(403).json({
+        message: "The flow  not found",
+      });
+    }
+
+    await flowModels.findByIdAndDelete(flowId);
+
+    res.status(200).json({
+      message: "Flow successfully deleted",
+    });
+  } catch (error) {
+    console.log("Error in deleting the flow", error);
+    res.status(500).json({
+      message: "Error in deleting flow",
+    });
+  }
+}
